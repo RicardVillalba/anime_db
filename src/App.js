3f835261b32f96fa35b5fcea0bf41ea1e04a3bc2
RicardVillalba <ricardsax@gmail.com>
2022-04-08 14:06:13 +0200

    getting data from the API in topAnime

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ const App = () => {
   const [animeList, SetAnimeList] = useState([]);
   const [topAnime, SetTopAnime] = useState([]);
   const [search, SetSearch] = useState("");
+
+  const GetTopAnime = async () => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/top/anime/1/bypopularity`
+    ).then((res) => res.json());
+
+    SetTopAnime(temp.top.slice(0, 10));
+  };
+
+  useEffect(() => {
+    GetTopAnime();
+  }, []);
+
+  console.log(topAnime);
+
   return (
     <div className="App">
       <Header />
