4dbc0febd6beee027e19c4270a70803a393c8023
RicardVillalba <ricardsax@gmail.com>
2022-04-08 13:46:57 +0200

    search form created in Mainanimecontent component

--- src/components/MainAnimeContent.js
+++ src/components/MainAnimeContent.js
@@ -3,10 +3,17 @@ import "./css/MainAnimeContent.css";
 const MainAnimeContent = () => {
   return (
     <div className="mainAnimeContainer">
-      <span>haibane renmei</span>
-      <span>tengen toppa</span>
-      <span>akebi chan</span>
-      <span>dragon ball</span>
+      <div className="mainAnimeHead">
+        <form className="searchAnime">
+          <input type="search" placeholder="search for an anime" required />
+        </form>
+      </div>
+      <div className="mainAnimeCards">
+        <span>haibane renmei</span>
+        <span>tengen toppa</span>
+        <span>akebi chan</span>
+        <span>dragon ball</span>
+      </div>
     </div>
   );
 };
