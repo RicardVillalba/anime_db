7e9a274d1fadba075589500efe50105e136a8480
RicardVillalba <ricardsax@gmail.com>
2022-04-08 17:36:09 +0200

    basic styles added

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -4,9 +4,9 @@ const AnimeCard = ({ anime }) => {
   return (
     <div className="cardContainer">
       <a href={anime.url} target="_blank" rel="noreferrer">
-        <figure>
-          <img src={anime.image_url} alt="anime image" />
-        </figure>
+        <div>
+          <img src={anime.image_url} alt="anime im" />
+        </div>
         <h3>{anime.title}</h3>
       </a>
     </div>
