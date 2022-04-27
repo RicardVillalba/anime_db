199944e4c14e7fd7f1ee8e3d72770b4c9a5b7e8e
RicardVillalba <ricardsax@gmail.com>
2022-04-27 14:09:45 +0200

    Animedetails changed if rendering manga information

--- src/components/AnimeDetails.js
+++ src/components/AnimeDetails.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "./css/AnimeDetails.css";
 
 const AnimeDetails = ({ anime }) => {
+  console.log(anime.episodes);
   return (
     <div className="animeDetailsContainer">
       <div className="animeDetailsHeader">
@@ -18,20 +19,43 @@ const AnimeDetails = ({ anime }) => {
             <p>{anime.synopsis}</p>
           </div>
           <div className="details">
-            <h4> episodes: </h4>
-            <p>{anime.episodes}</p>
+            {anime.episodes === undefined ? (
+              <div>
+                <h4> chapters: </h4>
+                <p>{anime.chapters}</p>
+              </div>
+            ) : (
+              <div>
+                <h4> episodes: </h4>
+                <p>{anime.episodes}</p>
+              </div>
+            )}
+
             <h4> score: </h4>
             <p>{anime.score}</p>
             <h4> start date: </h4>
             <p>{anime.start_date.slice(0, 10)}</p>
             <h4> end date: </h4>
             <p>{anime.end_date.slice(0, 10)}</p>
-            <h4> emision: </h4>
+            <h4>type: </h4>
             <p>{anime.type}</p>
-            <h4> rated: </h4>
-            <p>{anime.rated}</p>
-            <h4> airing: </h4>
-            <p>{anime.airing === true ? "yes" : "no"}</p>
+            {anime.rated === undefined ? null : (
+              <div>
+                <h4> rated </h4>
+                <p>{anime.rated}</p>
+              </div>
+            )}
+            {anime.airing === undefined ? (
+              <div>
+                <h4> publishing: </h4>
+                <p>{anime.publishing === true ? "yes" : "no"}</p>
+              </div>
+            ) : (
+              <div>
+                <h4> airing: </h4>
+                <p>{anime.airing === true ? "yes" : "no"}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
